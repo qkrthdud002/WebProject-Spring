12a95b06d95345e3ab16823bedc024c063b9d6a2
qkrthdud002 <qkrthdud002@gmail.com>
2022-09-14 11:32:19 +0900

    Spring 수업

---
+++ projectBuild/src/main/java/study/spring/projectBuild/Application1.java
@@ -0,0 +1,8 @@
+package study.spring.projectBuild;
+
+public class Application1 {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello();
+    }
+}
---
+++ projectBuild/src/main/java/study/spring/projectBuild/Hello.java
@@ -0,0 +1,7 @@
+package study.spring.projectBuild;
+
+public class Hello {
+    public void sayHello() {
+        System.out.println("Hello Spring");
+    }
+}
