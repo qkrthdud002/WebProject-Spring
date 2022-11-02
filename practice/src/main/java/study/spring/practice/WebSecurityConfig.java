9c0b4fc2d0ca32652b1b1af2fd412dd5d2a971d0
qkrthdud002 <qkrthdud002@gmail.com>
2022-11-02 13:21:38 +0900

    Spring Security 1102

--- practice/src/main/java/study/spring/practice/WebSecurityConfig.java
+++ practice/src/main/java/study/spring/practice/WebSecurityConfig.java
@@ -2,10 +2,15 @@ package study.spring.practice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
@@ -15,9 +20,16 @@ public class WebSecurityConfig {
         http.authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/").permitAll()
-                .antMatchers("/user/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/register").permitAll()
+                .antMatchers("/h2-console").permitAll()
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
