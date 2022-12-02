7c0541e2a7cd295677c7a178c3ecbf114bf4b129
qkrthdud002 <qkrthdud002@gmail.com>
2022-12-02 22:37:24 +0900

    20221129 - spring boot 과제(board)

--- boardsubject/src/main/java/com/subject/board/boardsubject/board/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.subject.board.boardsubject.board.repository;
-
-import com.subject.board.boardsubject.board.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-
-}
